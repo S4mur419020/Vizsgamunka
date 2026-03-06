dcff1c4f31d4497179c03c0d1d74a618220453bf
S4mur419020 <muraitomi04@gmail.com>
2026-03-06 11:49:32 +0100

    admin oldal igazitas

--- frontend/src/Components/admin/AdminSidebar.js
+++ frontend/src/Components/admin/AdminSidebar.js
@@ -6,9 +6,7 @@ function AdminSidebar() {
         <aside className="admin-sidebar">
             <NavLink to="/admin/products" className={getNavClass}>Termékek</NavLink>
             <NavLink to="/admin/orders" className={getNavClass}>Rendelések</NavLink>
-            <NavLink to="/admin/customers" className={getNavClass}>Vásárlók</NavLink>
             <NavLink to="/admin/discounts" className={getNavClass}>Kedvezmények</NavLink>
-            <NavLink to="/admin/settings" className={getNavClass}>Beállítások</NavLink>
             <NavLink to="/admin/users" className={getNavClass}>Felhasználók</NavLink>
             <NavLink to="/admin/statistics" className={getNavClass}>Statisztikák</NavLink>
         </aside>
